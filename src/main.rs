08acd5fa61224092db414ba5e3497daaafd403b4
x64neco <x64neco@gmail.com>
2022-01-21 13:51:43 +0900

    軽く実装

--- src/main.rs
+++ src/main.rs
@@ -2,50 +2,106 @@ extern crate crypto;
 use crypto::sha2::Sha256;
 use crypto::digest::Digest;
 
-use chrono::{Utc, Local, DateTime, Date};
 
+use std::fs::File;
+use std::io::{Write};
 
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
 
-#[derive(Debug, Serialize, Deserialize)]
+use chrono::{Utc, Local, DateTime, Date};
+
+
+#[derive(Debug, Serialize, Deserialize,Clone,)]
 struct Transaction {
     sender: String,
     recipient: String,
     amount: i64,
 }
 
-
-struct Block {
-    index: u64,
+#[derive(Serialize, Deserialize, Debug,Clone,)]
+struct Block 
+{
+    index: usize,
     hash: String,
-    block: [i32; 6], // 126
     transactions: Transaction, //168
-    timestamp:Date<Utc>  //DateTime<Utc>
+    timestamp:DateTime<Local>  //DateTime<Utc>
+}
+#[derive(Debug, Serialize, Deserialize,)]
+struct Chain {
+    chain: Vec<Block>
 }
 
 
-
-
-pub trait Chain {
-    fn crates(&self) -> String;
-    fn crates_hash(&self) -> String;
-
+trait Chains {
+    fn save(&self) -> std::io::Result<()>;
 }
 
 
-impl Chain for Block {
-    fn crates(&self) -> String {
-        String::from("a")
+impl Chains for Chain {
+    fn save(&self) ->  std::io::Result<()> {
+
+        let mut f = File::create("Block.json")?;
+        let result = match f.write_all(serde_json::to_string(&self.chain).unwrap().as_bytes()){
+            Ok(_) => Ok(()),
+            Err(e) => return Err(e),
+        };
+
+        result
     }
+}
 
-    fn crates_hash(&self) -> String {
-        let index:String = self.index.to_string();
-        let timestamp:String = self.timestamp.to_string();
-        let hash:String = self.hash.to_string();
-        let transactions:String = serde_json::to_string(&self.transactions).unwrap();
+
+trait Blocks {
+    fn create_block(&self,chain:&mut Vec<Block>) -> Block;
+    fn create_hash(&self,block:Block) -> String;
+
+}
+
+impl Blocks for Transaction {
+
+    fn create_block(&self,chain:&mut Vec<Block>) -> Block {
+
+        if chain.len() == 0 {
+            chain.push(Block {
+                index: 0,
+                hash: String::from(""),
+                transactions:Transaction {
+                    sender: String::from("x64neco"),
+                    recipient: String::from("yato"),
+                    amount: 0
+                },
+                timestamp:Local::now(), //Utc::now(),
+            });
+        }
+        
+
+        let block = Block {
+            index: chain.len(),
+            hash: self.create_hash(chain[chain.len() - 1].clone()),
+            transactions: Transaction {
+                sender: self.sender.clone(),
+                recipient:  self.recipient.clone(),
+                amount: self.amount,
+            },
+            timestamp:Local::now(), //Utc::now(),
+        };
+
+        chain.push(block.clone());
+        
+
+        block
+
+    }
+    
+
+    fn create_hash(&self,block:Block) -> String {
+        let index:String = block.index.to_string();
+        let timestamp:String = block.timestamp.to_string();
+        let hash:String = block.hash.to_string();
+        let transactions:String = serde_json::to_string(&block.transactions).unwrap();
         
         let input: String = index + &timestamp + &hash + &transactions;
 
@@ -60,21 +116,30 @@ impl Chain for Block {
 
 fn main() {
 
-
-    let block = Block {
-        index: 0,
-        hash: String::from(""),
-        block:[1,2,5,6,8,6],
-        transactions:Transaction {
-            sender: String::from("x64neco"),
-            recipient: String::from("yato"),
-            amount: 1200
-        },
-        timestamp:Utc::today(), //Utc::now(),
-    };
+    let mut chain:Vec<Block> = Vec::new();
 
 
-    println!("{:?}",block.crates_hash());
+    let block1 = Transaction {sender: String::from("x64neco"),recipient: String::from("yato"),amount: 1200};
+    let block2 = Transaction {sender: String::from("a"),recipient: String::from("s"),amount: 120};
+    let block3 = Transaction {sender: String::from("a"),recipient: String::from("s"),amount: 120};
+
+
+
+    println!("{:?}",block1.create_block(&mut chain));
+    block2.create_block(&mut chain);
+    block3.create_block(&mut chain);
+
+
+    //println!("{:?}",block2.create_block(&mut chain));
+
+    let chain = Chain{chain:chain};
+
+    println!("{:?}",chain.save());
+
+
+    //println!("{:?}",Local::now())
+
+    
 
     //println!("{:?}",serde_json::to_string(&Transaction{sender: String::from("x64neco"),recipient: String::from("yato"),amount: 1200}).unwrap())
 
